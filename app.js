75240894289b6d50eb44a784fd1d349e732e9d93
Nisa Parveen Shaikh <kuroibarakarose@gmail.com>
2024-07-24 15:38:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -151,6 +151,7 @@ app.use((err, req, res, next) =>{
     res.status(statusCode).render('errors', { err });
 })
 
-app.listen(3000, () => {
-    console.log('Serving on port: 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`erving on port: ${port}`);
 })
